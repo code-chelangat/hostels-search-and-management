8daaef8570b9dd1bab149b1697764b354f83d631
code-chelangat <caroroline.chelangat@student.moringaschool.com>
2022-09-06 17:32:02 +0300

    added form2

---
+++ index.js
@@ -0,0 +1,10 @@
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("http://localhost:3000/hostels", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+    
