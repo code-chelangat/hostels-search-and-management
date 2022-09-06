422743f1b64eed20d42e93b3135584ae6725eb81
code-chelangat <caroroline.chelangat@student.moringaschool.com>
2022-09-06 15:23:15 +0300

    added db json

---
+++ db.js
@@ -0,0 +1,41 @@
+{
+    "hostels": [
+      { "id": 1,
+        "name": "Marku Hostels ",
+        "location": "Thika"
+        
+      },
+  
+      { "id": 2,
+        "name": "Callus Hostels ",
+        "location": "Nakuru"
+       
+      },
+  
+      { "id": 3,
+      "name": "siri Homes ",
+      "location":"Nairobi"
+      
+  
+      },
+  
+      { "id": 4,
+        "name": "hunza Hostels ",
+        "location": "Eldoret"
+        
+      },
+  
+      { "id": 5,
+        "name": "Ginas Hostels ",
+       "location":"Mombasa"
+        
+      },
+  
+      { "id": 6,
+        "name": "nzana court ",
+        "location": "Kericho"
+       
+      }
+    ]
+  }
+  
