c486de40a52698b63402ced80706a830d4660c44
code-chelangat <caroroline.chelangat@student.moringaschool.com>
2022-09-19 15:38:45 +0300

    added container

--- index.js
+++ index.js
@@ -2,29 +2,28 @@
 //     method: 'GET',
 //     redirect: 'follow'
 //   };
-  
+
 //   fetch("http://localhost:3000/hostels", requestOptions)
 //     .then(response => response.text())
 //     .then(result => console.log(result))
 //     .catch(error => console.log('error', error));
 
-const button = document.getElementById("name");
-fetch("http://localhost:3000/hostels")
-.then((Response) => {
-  if (Response.ok){
-    return Response.json()
-  }else{
-    throw new Error("data not fetched")
-  }
-})
-.then((Response)=>{
-  button.addEventListener("click",function(){
-    Response.forEach(hostel => {
-      document.getElementById("infor").innerHTML= hostel.name
-      
-    });
-    
-    console.log (Response)
+const button = document.getElementById("Marku");
+fetch("http://localhost:3000/hostels/1")
+  .then((Response) => {
+    if (Response.ok) {
+      return Response.json();
+    } else {
+      throw new Error("data not fetched");
+    }
   })
-
-})
+  .then((Response) => {
+    button.addEventListener("click", function () {
+     
+      console.log(Response);
+      const name = document.getElementById("hostel-name");
+      const location = document.getElementById("hostel-location");
+      name.innerHTML = Response.name;
+      location.innerHTML = Response.location;
+    });
+  });
