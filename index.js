0586975701c4f3460c3de29a63ea7aaa92b5bd01
code-chelangat <caroroline.chelangat@student.moringaschool.com>
2022-09-19 13:30:25 +0300

    added js element

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-let  requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
+// let  requestOptions = {
+//     method: 'GET',
+//     redirect: 'follow'
+//   };
   
-  fetch("http://localhost:3000/hostels", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+//   fetch("http://localhost:3000/hostels", requestOptions)
+//     .then(response => response.text())
+//     .then(result => console.log(result))
+//     .catch(error => console.log('error', error));
+
+const button = document.getElementById("name");
+fetch("http://localhost:3000/hostels")
+.then((Response) => {
+  if (Response.ok){
+    return Response.json()
+  }else{
+    throw new Error("data not fetched")
+  }
+})
+.then((Response)=>{
+  button.addEventListener("click",function(){
+    Response.forEach(hostel => {
+      document.getElementById("infor").innerHTML= hostel.name
+      
+    });
+    
+    console.log (Response)
+  })
+
+})
